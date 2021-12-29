6dc9170ea539e8c192a9c60a767be36f5aa7eded
William Owens <williamlewisowens@gmail.com>
2021-12-28 17:39:06 -0700

    adding start of day 4

---
+++ Day04/main.py
@@ -0,0 +1,9 @@
+import os
+
+if __name__ == "__main__":
+    dir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(dir, "input.txt")) as file:
+        _input = file.read().splitlines()
+
+    # print(solution1(_input))
+    # print(solution2(_input))
